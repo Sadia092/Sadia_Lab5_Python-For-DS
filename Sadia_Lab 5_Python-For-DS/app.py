1a696ca1f87356d4beb2679ad8bb1fe910949e72
Sadia Fatima <119720645+Sadia092@users.noreply.github.com>
2024-01-14 16:42:44 +0530

    Add files via upload

---
+++ Sadia_Lab 5_Python-For-DS/app.py
@@ -0,0 +1,34 @@
+from sklearn.preprocessing import StandardScaler
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+# open and load the pickle file provided in read mode.
+model = pickle.load(open('predict_model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        age_of_the_car = request.form['age_of_the_car']
+        Present_Price = float(request.form['Present_Price'])
+        Kms_Driven = int(request.form['Kms_Driven'])
+        Fuel_Type = request.form['Fuel_Type']
+        Seller_Type = request.form['Seller_Type']
+        Transmission = request.form['Transmission']
+        Owner = request.form['Owner']
+
+        prediction = model.predict(
+            [[age_of_the_car, Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner]])
+        output = round(prediction[0], 2)
+        print("Value of prediction_text:", output)
+        return render_template('index.html', prediction_text='You can sell your car at {} lakhs'.format(output))
+
+# Set the TEMPLATES_AUTO_RELOAD option before running the app
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+if __name__ == "__main__":
+    app.run(debug=True)
